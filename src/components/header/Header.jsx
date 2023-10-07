c62303d2e97c0e796c2564a0e83fb0402bf47772
nyitrai100 <nyitrai100@gmail.com>
2023-10-07 13:57:03 +0100

    CTA componenet done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
 
 const header = () => {
   return (
-    <div>header</div>
+   <header>
+    <div className="container header_container">
+      <h5>Hello I'm</h5>
+      <h1>Bence Nyitrai</h1>
+      <h5 className='text-light'>Web Developer</h5>
+      <CTA />
+    </div>
+   </header>  
   )
 }
 
