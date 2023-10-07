25b298d4d41be9bd4c0c492c5bd4cee60d84f291
nyitrai100 <nyitrai100@gmail.com>
2023-10-07 15:41:54 +0100

    about me section done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import'./about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get to Know</h5>
+      <h2>About ME</h2>
+
+      <div className="container about_container">
+
+          <div className="about_me">
+              <div className="about_me-image">
+                <img src={ME} alt="About Image" />
+                </div>
+          </div>
+
+          <div className="about_content">
+              <div className="about_cards">
+                <article className='about_card'>
+                    <FaAward className='about_icon'/>
+                    <h5>Experience</h5>
+                    <small> 3+ Years Working</small>
+                </article>
+
+                <article className='about_card'>
+                    <FiUsers className='about_icon'/>
+                    <h5>Clients</h5>
+                    <small> 200+ Worldwide</small>
+                </article>
+
+                <article className='about_card'>
+                    <VscFolderLibrary className='about_icon'/>
+                    <h5>Projects</h5>
+                    <small> 100+ Completed Projects</small>
+                </article>
+              </div>
+
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, commodi mollitia voluptatibus voluptas recusandae perferendis quae soluta quo reprehenderit sit ea, enim minima dicta exercitationem? Minima, nisi corrupti? Explicabo, molestias?</p>
+              <a href="#contact" className='btn btn-primary'> Let's Talk</a>
+          </div>
+      </div>
+
+    </section>
   )
 }
 
