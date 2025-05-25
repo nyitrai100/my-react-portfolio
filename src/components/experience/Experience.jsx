502024cbbb4ea2ba1264cc474c5d4c243c7acc8a
nyitraibence <nyitrai100@gmail.com>
2025-05-25 11:17:25 +0100

    updated expreince

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -77,31 +77,12 @@ const Experience = () => {
                   <h4>React</h4>
                   </div>
             </article>
-            <article className='experience_details'>
-                  <TbBrandNextjs className='experience_details-icon'/>
-                  <div>
-                  <h4>NextJS</h4>
-                  </div>
-            </article>
-      
         </div>
       </div>
 
       <div className="experience_backend">
       <h3>Backend Development</h3>
         <div className="experience_content">
-            <article className='experience_details'>
-                  <BiLogoNodejs className='experience_details-icon'/>
-                <div>
-                   <h4>NodeJS</h4>
-                </div>
-            </article>
-            <article className='experience_details'>
-                  <BiLogoMongodb className='experience_details-icon'/>
-                 <div>
-                 <h4>MongoDB</h4>
-                 </div>
-            </article>
             <article className='experience_details'>
                   <LiaPhp className='experience_details-icon'/>
                   <div>
@@ -118,6 +99,12 @@ const Experience = () => {
                   <TbBrandCSharp className='experience_details-icon' />
                  <div>
                  <h4>C#</h4>
+                 </div>
+            </article>
+            <article className='experience_details'>
+                  <TbBrandCSharp className='experience_details-icon' />
+                 <div>
+                 <h4>Azure</h4>
                  </div>
             </article>
         </div>
