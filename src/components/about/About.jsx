a01ae4d31f3b4f7635105f8588c69b0ccfa52e1a
nyitraibence <nyitrai100@gmail.com>
2026-07-02 17:53:15 +0200

    updated about text

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -44,12 +44,14 @@ const About = () => {
                 
                 <h4 className='welcome'>Welcome!</h4>   
                   <p> My name is Bence Nyitrai,  
-                      I’m a Web Programming with Cybersecurity student at the University of Huddersfield, UK.  
-                      <br/>I currently work as a Junior Software Developer at Cortex, based in the Channel Islands.  
-                      <br/>I've gained hands-on experience in both Front-end and Back-end development using C#, Blazor, JavaScript, and SQL.  
-                      I enjoy solving problems and continuously improving through real-world projects and teamwork.  
-                      <br/>If you're interested in connecting or have any questions, feel free to reach out.  
-                      Thanks for visiting my portfolio—I look forward to hearing from you!  
+                    My name is Bence Nyitrai, and I recently graduated with First Class Honours in Web Programming with Cybersecurity from the University of Huddersfield, UK. 
+                    During my studies, I was awarded the Departmental Prize for the Best Web and Information Systems Student and the Best Overall Performance on the Final Year Project in Web and Information Systems. 
+                    I have one year of professional experience as a Junior Software Developer at Cortex, based in the Channel Islands. 
+                    Throughout my studies and professional career, I have gained hands-on experience in both front-end and back-end development using C#, Blazor, JavaScript, and SQL.
+                    I enjoy solving complex problems, building high-quality software, and continuously improving through real-world projects and teamwork. 
+                    I am now seeking new opportunities where I can continue to grow as a software developer. 
+                    If you're interested in connecting or have any questions, feel free to reach out. 
+                    Thanks for visiting my portfolio, and I look forward to hearing from you!
                   </p>
               
           </div>
